51818607953cdfab19fcaed5f6f5c598f6049744
Felipe Ruhland <firuhland@gmail.com>
2014-01-23 17:06:55 -0200

    example mojo

---
+++ src/main/java/com/github/geub/plugin/maven/example/ExampleMojo.java
@@ -0,0 +1,15 @@
+package com.github.geub.plugin.maven.example;
+
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+
+/**
+ * @goal example
+ */
+public class ExampleMojo extends AbstractMojo {
+
+	@Override
+	public void execute() throws MojoExecutionException {
+		System.out.println("Example");
+	}
+}
